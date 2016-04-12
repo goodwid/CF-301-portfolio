33cb370958c203ea7985862a96ec8e051a60afd0
goodwid <goodwid@gmail.com>
2016-04-11 21:17:48 -0700

    Added function to display project data, project is showing data on screen now.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 var portfolioItems = [];
 
 function Project (opts) {
@@ -7,14 +8,28 @@ function Project (opts) {
     this.repoURL = opts.repoURL;
     this.image = opts.image;
     this.completedOn = opts.completedOn;
-
 }
 
-Project.prototype.toHtml = function () {
+Project.prototype.toHtml = function() {
     var $newProject = $('article.template').clone();
 
-    $newProject.find('h3').text(this.title);
-    $newProject.find('img').attr('src',this.image);
-    $newProject.find('p').html(this.desc);
-    $newProject.
-}
+    $newProject.find('.project-title').text(this.title);
+    $newProject.find('.project-image').attr('src',this.image);
+    $newProject.find('.project-desc').html(this.desc);
+    $newProject.find('.repo-url').attr('href',this.repoURL);
+    $newProject.find('.project-url').attr('href',this.projectURL);
+    $newProject.find('.project-date').text(this.completedOn);
+
+    $newProject.removeClass('template');
+    $newProject.draggable();
+
+    return $newProject;
+};
+
+rawData.forEach(function(ele) {
+    portfolioItems.push(new Project(ele));
+});
+
+portfolioItems.forEach(function(a) {
+    $('#projects').append(a.toHtml());
+});
--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,7 @@
 var rawData = [
     {
         title:      'Conquer It!',
-        projectURL; 'http://goodwid.github.io/ExposureTherapy/',
+        projectURL: 'http://goodwid.github.io/ExposureTherapy/',
         desc:       'Exposure therapy for people suffering from arachnophobia.',
         repoURL:    'https://github.com/goodwid/ExposureTherapy',
         image:      '',
