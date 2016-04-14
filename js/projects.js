bec4ad35ada9ff0728789817fc900e13d6967077
goodwid <goodwid@gmail.com>
2016-04-14 16:29:12 -0700

    Refactored CSS into SMACSS

--- js/projects.js
+++ js/projects.js
@@ -2,46 +2,46 @@ var rawData = [
   {
     title:      'Conquer It!',
     projectURL: 'http://goodwid.github.io/ExposureTherapy/',
-    desc:       'Exposure therapy for people suffering from arachnophobia.',
     repoURL:    'https://github.com/goodwid/ExposureTherapy',
     image:      'images/placeholder.jpg',
     category:   'real',
-    completedOn:'2016-04-08'
+    completedOn:'2016-04-08',
+    desc:       'Exposure therapy for people suffering from arachnophobia.'
   },
   {
     title:      'Pizza 3001',
     projectURL: 'http://goodwid.github.io/CF-201-week2/pizza',
-    desc:       'Pizza tracking demo',
     repoURL:    'https://github.com/goodwid/CF-201-week2/tree/master/pizza',
     image:      'images/placeholder.jpg',
     category:   'real',
-    completedOn:'2016-03-25'
+    completedOn:'2016-03-25',
+    desc:       'Pizza tracking demo',
   },
   {
     title:      'Project A',
     projectURL: '#project',
-    desc:       'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     repoURL:    '#repo',
     image:      'images/placeholder.jpg',
     category:   'fake',
-    completedOn:'2016-03-25'
+    completedOn:'2016-03-25',
+    desc:       '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>'
   },
   {
     title:      'Project B',
     projectURL: '#project',
-    desc:       'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     repoURL:    '#repo',
     image:      'images/placeholder.jpg',
     category:   'fake',
-    completedOn:'2016-03-25'
+    completedOn:'2016-03-25',
+    desc:       'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
   },
   {
     title:      'Project C',
     projectURL: '#project',
-    desc:       'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     repoURL:    '#repo',
     image:      'images/placeholder.jpg',
     category:   'unreal',
-    completedOn:'2016-03-25'
+    completedOn:'2016-03-25',
+    desc:       'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
   },
 ];
