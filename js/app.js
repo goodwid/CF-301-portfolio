026c1a9550d6c43d9f2062c8e5ba75f73e94797e
goodwid <goodwid@gmail.com>
2016-04-14 15:05:21 -0700

    Changed the %##^* indentation.

--- js/app.js
+++ js/app.js
@@ -2,35 +2,35 @@
 var portfolioItems = [];
 
 function Project (opts) {
-    this.title = opts.title;
-    this.projectURL = opts.projectURL;
-    this.desc = opts.desc;
-    this.repoURL = opts.repoURL;
-    this.image = opts.image;
-    this.completedOn = opts.completedOn;
-    this.category = opts.category;
+  this.title = opts.title;
+  this.projectURL = opts.projectURL;
+  this.desc = opts.desc;
+  this.repoURL = opts.repoURL;
+  this.image = opts.image;
+  this.completedOn = opts.completedOn;
+  this.category = opts.category;
 }
 
 Project.prototype.toHtml = function() {
-    var $newProject = $('article.template').clone();
+  var $newProject = $('article.template').clone();
 
-    $newProject.find('h3').text(this.title);
-    $newProject.attr('data-category', this.category);
-    $newProject.find('img').attr('src',this.image);
-    $newProject.find('p:last').html(this.desc);
-    $newProject.find('a').attr('href',this.projectURL);
-    $newProject.find('p a').attr('href',this.repoURL);
-    $newProject.find('date').text(this.completedOn);
+  $newProject.find('h3').text(this.title);
+  $newProject.attr('data-category', this.category);
+  $newProject.find('img').attr('src',this.image);
+  $newProject.find('p:last').html(this.desc);
+  $newProject.find('a').attr('href',this.projectURL);
+  $newProject.find('p a').attr('href',this.repoURL);
+  $newProject.find('date').text(this.completedOn);
 
-    $newProject.removeClass('template');
+  $newProject.removeClass('template');
 
-    return $newProject;
+  return $newProject;
 };
 
 rawData.forEach(function(ele) {
-    portfolioItems.push(new Project(ele));
+  portfolioItems.push(new Project(ele));
 });
 
 portfolioItems.forEach(function(a) {
-    $('#projects').append(a.toHtml());
+  $('#projects').append(a.toHtml());
 });
--- js/projectView.js
+++ js/projectView.js
@@ -3,54 +3,54 @@
 var projectView = {};
 
 projectView.populateFilter = function() {
-    var cats = [];
-    var category;
-    var optionTag = '';
-    $('article').each(function () {
-        if (!$(this).hasClass('template')) {
-            category = $(this).attr('data-category');
-            if (cats.indexOf(category) === -1) {  // Only add to categories array if it's not already in there.
-                cats.push(category);
-            }
-        }
-    });
-    cats.forEach(function (val) {
-        optionTag = '<option value="' + val + '">' + val + '</option>';
-        $('#category-filter').append(optionTag);
-    });
+  var cats = [];
+  var category;
+  var optionTag = '';
+  $('article').each(function () {
+    if (!$(this).hasClass('template')) {
+      category = $(this).attr('data-category');
+      if (cats.indexOf(category) === -1) {  // Only add to categories array if it's not already in there.
+        cats.push(category);
+      }
+    }
+  });
+  cats.forEach(function (val) {
+    optionTag = '<option value="' + val + '">' + val + '</option>';
+    $('#category-filter').append(optionTag);
+  });
 };
 
 projectView.handleFilter = function() {
-    $('#category-filter').on('change', function() {
-        if ($(this).val()) {
-            $('#projects article').each(function() {
-                $(this).hide();
-            });
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('#projects article').each(function() {
+        $(this).hide();
+      });
 
-            $('#projects article').filter(function() {
-                return $(this).attr('data-category') == $('#category-filter').val();
-            }).show();
-        } else {
-            $('#projects article:not(.template)').each(function() {
-                $(this).show();
-            });
-        }
-    });
+      $('#projects article').filter(function() {
+        return $(this).attr('data-category') == $('#category-filter').val();
+      }).show();
+    } else {
+      $('#projects article:not(.template)').each(function() {
+        $(this).show();
+      });
+    }
+  });
 };
 
 projectView.handleMainNav = function() {
-    $('nav ul').on('click','.tab', function(event) {   // Class needed here to differentiate li elements from other links in the nav bar.
-        event.preventDefault();
-        $('main > section').each(function() {
-            $(this).hide();
-        });
-        $($(this).find('a').attr('href')).show();
+  $('nav ul').on('click','.tab', function(event) {   // Class needed here to differentiate li elements from other links in the nav bar.
+    event.preventDefault();
+    $('main > section').each(function() {
+      $(this).hide();
     });
+    $($(this).find('a').attr('href')).show();
+  });
 };
 
 $(document).ready(function() {
-    projectView.populateFilter();
-    projectView.handleFilter();
-    projectView.handleMainNav();
-    $('#about').hide();
+  projectView.populateFilter();
+  projectView.handleFilter();
+  projectView.handleMainNav();
+  $('#about').hide();
 });
