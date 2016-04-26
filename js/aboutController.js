76f00f67ec244671181cea664e8e0104a03388e2
goodwid <goodwid@gmail.com>
2016-04-26 14:15:05 -0700

    Formatting change

--- js/aboutController.js
+++ js/aboutController.js
@@ -5,6 +5,9 @@
     $('main > section').hide();
     $('#about').fadeIn();
     $('header p').css('opacity','0');
+    if($('.icon-menu').is(':visible')) {
+      $('nav ul').hide();
+    }
   };
 
   module.aboutController = aboutController;
--- js/projectController.js
+++ js/projectController.js
@@ -7,6 +7,9 @@
     $('main > section').hide();
     $('header p').css('opacity','1');
     $('#projects').fadeIn();
+    if($('.icon-menu').is(':visible')) {
+      $('nav ul').hide();
+    }
   };
 
   module.projectController = projectController;
